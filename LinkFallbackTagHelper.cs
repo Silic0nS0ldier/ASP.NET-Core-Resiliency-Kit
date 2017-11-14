7b31934a07b5b88cca4cbd3bf68703f2f7281a0d
Jordan Mele <jordan.mele@outlook.com.au>
2017-11-14 17:43:34 +1100

    Initial

---
+++ LinkFallbackTagHelper.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Html;
+
+/*
+Use onerror event to track load failure.
+Support error reporting via AJAX and callbacks.
+https://www.davepaquette.com/archive/2015/06/22/creating-custom-mvc-6-tag-helpers.aspx
+https://docs.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.razor.taghelpers.taghelper?view=aspnetcore-2.0
+http://www.dotnetcurry.com/aspnet-mvc/1266/using-tag-helpers-aspnetmvc-6-core-1
+https://docs.microsoft.com/en-us/aspnet/core/mvc/views/tag-helpers/intro
+*/
+
+namespace Fallback.AspNetCore
+{
+    [HtmlTargetElement("link", Attributes = FallbackHrefAttributeName)]
+    public class LinkFallbackTagHelper : ITagHelper
+    {
+        private const string FallbackHrefAttributeName = "fallback-href";
+
+        [HtmlAttributeName(FallbackHrefAttributeName)]
+        public string FallbackHref { get; set; }
+
+        private bool firstInvocation = false;
+
+        // No time dependencies, and so can be run right off the bat.
+        public int Order => 0;
+
+        public void Init(TagHelperContext context)
+        {
+            // Check for previous invocations. If there is none, this invocation must provide the fallback method.
+            if (!context.Items.ContainsKey("FallbackFirstFound")) {
+                context.Items.Add("FallbackFirstFound", true);
+                firstInvocation = true;
+            }
+        }
+
+        public Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (firstInvocation) {
+/*
+// Recycles tag with fallback used upon download failure.
+function fallback(sender, type, url, phoneHomeUrl, callback) {
+    // Ping home about error if requested.
+    if (phoneHome) {
+        var req = new XMLHttpRequest();
+        req.open("POST", phoneHomeUrl, true);
+        req.send(JSON.stringify({
+            message: ""
+        })
+    }
+    // Replace initial url
+    if (type == "css") sender.href = url;
+    else if (type == "js") sender.src = url;
+    else throw "Type unsupported";
+
+    // Prevent loop, and log errors if requested.
+    //alt onerror
+    //
+
+    // Execute callback, if defined.
+
+    // Re-plug.
+    if (sender.replaceWith) sender.replaceWith(sender);
+    else sender.parentNode.replaceChild(sender, sender);
+}
+
+function fallbackFailure(sender, phoneHome, callback) {
+
+}
+*/
+                output.PreElement.AppendHtml(new Microsoft.AspNetCore.Html.HtmlString(
+@"<script type=""application/javascript"">
+function fallback(sender, type) {
+    switch (type) {
+        case ""css"":
+        case ""js"":
+    }
+}
+</script>
+"));
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+Fallback.AspNetCore
+===================
+
+CDNs are great, but (usually) outside your control. Fallback systems exist, but tend to hinder flexibility by requiring a check of some known property (such as the fallback feature of LinkTagHelper in ASP.Net Core) or simply force an independent JS script to handle everything (such as RequireJS, and FallbackJS).
+
+**Fallback.AspNetCore** puts ASP.Net Core's tag helper system to work for you. Simply provide the alternative source(s) for any external script or link tag. That's it. The necessary script (weighing in at a tiny xKB) will be inserted once just before the first place a fallback is provided. Even better, you can arrange for reporting on failure, and execute your own script on failure, making it easier to track issues as they pop up.
+
+Admittedly, there are some fantastic JS loaders out there. Whats best is really depends on your project.
