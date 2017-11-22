1344991ba26717cfc5f75f9efe721b94db6cf9c5
Jordan Mele <jordan.mele@outlook.com.au>
2017-11-22 21:50:02 +1100

    Finished unit tests

--- test/FallbackTagHelperTest.cs
+++ test/FallbackTagHelperTest.cs
@@ -70,12 +70,14 @@ namespace Fallback.AspNetCore.Test
 
             // Inspect output
 
+            // Script should be embedded above tag
+            Assert.Equal(this.FallbackScript, env.output.PreElement.GetContent());
             // Tag name should be equal to input
             Assert.Equal(tagName, env.output.TagName);
             // Attribute onerror should be equal.
             TagHelperAttribute onerrorResult;
             env.output.Attributes.TryGetAttribute("onerror", out onerrorResult);
-            Assert.Equal($@"fallback(this, ""{fallbackUrls}"", ""{reportUrl}"");", onerrorResult.Value);
+            Assert.Equal($@"fallback(this, ""{fallbackUrls}"", ""{reportUrl}"");", onerrorResult?.Value);
             // Attribute fallback-urls should not exist.
             Assert.False(env.output.Attributes.ContainsName("fallback-urls"));
             // Attribute fallback-report-url should not exist.
@@ -83,10 +85,76 @@ namespace Fallback.AspNetCore.Test
         }
 
         // Twice second
-        //[Theory]
-        public void FallbackUsedTwiceTest()
+        // TODO: Jordan Mele - Ideally there should be more unique data to ensure everything is working correctly, but it does the trick for now.
+        // TODO: Jordan Mele - This test is messy, and should be cleaned up.
+        [Theory]
+        [InlineData("link", "http://www.example.com/1.css~http://www.example.com/2.css")]
+        [InlineData("script", "http://www.example.com/3.js", "http://www.example.com/report/resourcefailure/")]
+        public void FallbackUsedTwiceTest(string tagName, string fallbackUrls, string reportUrl = "")
         {
+            // Create environment
+            var env1 = EnvironmentGenerator(tagName, fallbackUrls, reportUrl);
 
+            // Construct helper 1
+            var fth1 = new FallbackTagHelper
+            {
+                // Apply attributes to emulate what ASP.NET Core does for us.
+                FallbackUrls = fallbackUrls,
+                FallbackReportUrl = reportUrl
+            };
+
+            // Initalise
+            fth1.Init(env1.context);
+
+            // Execute
+            fth1.Process(env1.context, env1.output);
+
+            // Extend environment
+            var env2 = EnvironmentGenerator(tagName, fallbackUrls, reportUrl, env1.context.Items);
+
+            // Construct helper 2
+            var fth2 = new FallbackTagHelper
+            {
+                // Apply attributes to emulate what ASP.NET Core does for us.
+                FallbackUrls = fallbackUrls,
+                FallbackReportUrl = reportUrl
+            };
+
+            // Initalise
+            fth2.Init(env2.context);
+
+            // Execute
+            fth2.Process(env2.context, env2.output);
+
+            // Inspection
+
+            // Inspect output of tag 1
+
+            // Script should be embedded above tag
+            Assert.Equal(this.FallbackScript, env1.output.PreElement.GetContent());
+            // Tag name should be equal to input
+            Assert.Equal(tagName, env1.output.TagName);
+            // Attribute onerror should be equal.
+            env1.output.Attributes.TryGetAttribute("onerror", out TagHelperAttribute onerrorResult);
+            Assert.Equal($@"fallback(this, ""{fallbackUrls}"", ""{reportUrl}"");", onerrorResult?.Value);
+            // Attribute fallback-urls should not exist.
+            Assert.False(env1.output.Attributes.ContainsName("fallback-urls"));
+            // Attribute fallback-report-url should not exist.
+            Assert.False(env1.output.Attributes.ContainsName("fallback-report-url"));
+
+            // Inspect output of tag 2
+
+            // Script should not be embedded above tag
+            Assert.NotEqual(this.FallbackScript, env2.output.PreElement.GetContent());
+            // Tag name should be equal to input
+            Assert.Equal(tagName, env2.output.TagName);
+            // Attribute onerror should be equal.
+            env2.output.Attributes.TryGetAttribute("onerror", out onerrorResult);
+            Assert.Equal($@"fallback(this, ""{fallbackUrls}"", ""{reportUrl}"");", onerrorResult?.Value);
+            // Attribute fallback-urls should not exist.
+            Assert.False(env2.output.Attributes.ContainsName("fallback-urls"));
+            // Attribute fallback-report-url should not exist.
+            Assert.False(env2.output.Attributes.ContainsName("fallback-report-url"));
         }
     }
 }
